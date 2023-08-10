6f4b6b60d82392fe168e9666fbf747166264a01c
seanc0der <sean.noko@gmail.com>
2023-08-10 21:10:34 +0200

    amended challenge1-iwa7.js by fixing the code and adding commenting as per the challenge requirements

--- challenge1-iwa7/challenge1-iwa7.js
+++ challenge1-iwa7/challenge1-iwa7.js
@@ -1,2 +1,5 @@
-const value = "3";
+// When concatenating a string (value) with a number (4)
+// the number will firstly be converted into a string literal by Javascript,
+// before concatenation occurs.
+const value = 3;
 console.log(value + 4 + value);
