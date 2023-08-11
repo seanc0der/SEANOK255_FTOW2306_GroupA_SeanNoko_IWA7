2d2308de9f581721fcf6e8e2e5c881b07a2daf74
seanc0der <sean.noko@gmail.com>
2023-08-11 10:54:20 +0200

    amended challenge2-iwa7.js by fixing the code, so that it produces the desired result, and adding commenting as per the challenge requirements

--- challenge2-iwa7/challenge2-iwa7.js
+++ challenge2-iwa7/challenge2-iwa7.js
@@ -1,4 +1,11 @@
+// The use of interpolation '${variable or expression}' within a normal string (single/double-quoted);
+// Interpolation doesn't work on a normal string, it is a feature only available on a template literal (which uses backticks).
+// Moreover the interpolation expression's syntax is incorrect, it should have been '${nickname || firstname}'.
+// As the || operator would be considered a string instead of an operator, if this expression was in a template literal.
+
 const nickname = "Timmy";
 const firstname = "Timothy";
+const greeting_message =
+	(nickname || firstname) && `Good Morning, ${nickname || firstname}!`;
 
-console.log("Good Morning, ${nickname} || {firstname}!");
+console.log(greeting_message || "Good Morning!");
