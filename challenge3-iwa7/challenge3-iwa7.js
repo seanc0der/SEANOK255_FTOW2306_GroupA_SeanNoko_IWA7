6721648f0a7fa07c51e5bfe5c416237ad9b81b58
seanc0der <sean.noko@gmail.com>
2023-08-11 17:26:42 +0200

    amended challenge3-iwa7.js by fixing and adding code, so that it produces the desired result.

--- challenge3-iwa7/challenge3-iwa7.js
+++ challenge3-iwa7/challenge3-iwa7.js
@@ -1,6 +1,6 @@
 const leoName = "Leo";
 const leoSurname = "Musvaire     ";
-const leoBalance = "-9394";
+const leoBalance = "-9394.";
 
 const sarahName = "Sarah    ";
 const sarahSurname = "Kleinhans";
@@ -9,11 +9,26 @@ const sarahBalance = "-4582.2";
 const divider = "----------------------------------";
 
 // Only change below this line
+const owedCalc = function (balance) {
+	const to_number = Math.abs(Number(balance));
+	const currency_type = { style: "currency", currency: "ZAR" };
+	const to_currency = to_number.toLocaleString("ZA", currency_type);
+	return to_currency.replace(",", ".");
+};
 
-const owed = parseInt("R" + leoBalance + sarahBalance);
-const leo = '{leoName} + {leoSurname} + "Owed" + "R" + {sarahBalance}';
-const sarah = '{leoName} + {surname} + "Owed" + "R" + {sarahBalance}';
-const total = "Total amount owed: ";
-const result = leo + sarah + divider + divider + total + owed + divider;
+const owed = owedCalc(Number(leoBalance) + Number(sarahBalance));
+const leo = `${leoName} ${leoSurname.trim()} (Owed: ${owedCalc(leoBalance)})`;
+const sarah = `${sarahName.trim()} ${sarahSurname} (Owed: ${owedCalc(
+	sarahBalance
+)})`;
+const total = `Total amount owed: ${owed}`;
+const result = `
+${leo}  
+${sarah}
+
+${divider} 
+  ${total} 
+${divider}
+`;
 
 console.log(result);
